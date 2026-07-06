4ab81b6f9f4bb64feef7b9baa617408b757bc39f
dangtuanminh8 <dangtuanminh1123@gmail.com>
2026-07-06 19:29:20 -0400

    Added clear all button, content when list is empty, and code comments Fixed formatting issues, removed unnessecary formatting code

--- paperweight.py
+++ paperweight.py
@@ -4,7 +4,6 @@ import threading
 import time
 from pynput import keyboard
 
-
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
@@ -15,27 +14,25 @@ class App(ctk.CTk):
         self.app_font = ctk.CTkFont(family="Calibri", size=13)
         self.copy_history = [] 
 
-        self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=1)
 
         # Title Bar
         self.title_bar = ctk.CTkFrame(
-                        master=self,
-                        height=35,
-                        fg_color="transparent",
-                        corner_radius=0
-                    )
-                
-        self.title_bar.grid_propagate(False)
-        self.title_bar.grid_columnconfigure(0, weight=1)
+            master=self,
+            height=35,
+            fg_color="transparent",
+            corner_radius=0
+        )
         
+        self.title_bar.grid_propagate(False)
         self.title_bar.grid(row=0, column=0, sticky="ew")
+        self.title_bar.grid_columnconfigure(1, weight=1)
+        self.title_bar.grid_rowconfigure(0, weight=1)
 
         self.close_btn = ctk.CTkButton(
             self.title_bar,
-            text="x",
-            width=35,
+            text="❌",
+            width=45,
             height=35,
             corner_radius=0,
             fg_color="transparent",
@@ -43,10 +40,36 @@ class App(ctk.CTk):
             command=self.destroy
         )
 
-        self.close_btn.grid(row=0, column=1, sticky="e")
+        self.close_btn.grid(row=0, column=1, sticky="ne")
 
         self.title_bar.bind("<ButtonPress-1>", self.drag_start)
         self.title_bar.bind("<B1-Motion>", self.drag_motion)
+
+        # Utility Bar
+        self.utility_bar = ctk.CTkFrame(
+            master=self,
+            height=35,
+            corner_radius=0,
+            fg_color="transparent",
+        )
+        
+        self.utility_bar.grid_propagate(False)
+        self.utility_bar.grid(row=1, column=0, sticky="ew")
+        self.utility_bar.grid_columnconfigure(1, weight=1)
+        self.utility_bar.grid_rowconfigure(0, weight=1)
+
+        self.clear_btn = ctk.CTkButton(
+            self.utility_bar,
+            text="Clear all",
+            font=self.app_font,
+            width=60,
+            height=30,
+            corner_radius=6,
+            fg_color="red",
+            hover_color="blue",
+            command=self.clear_content
+        )
+        self.clear_btn.grid(row=0, column=1, sticky="e", padx=10, pady=6)
 
         # Copy Content
         self.copy_content_frame = ctk.CTkScrollableFrame(
@@ -55,10 +78,11 @@ class App(ctk.CTk):
             height=320,
             fg_color="transparent"
         )
-        
-        self.copy_content_frame.grid(row=1, column=0, pady=16, sticky="ew")
+        self.copy_content_frame.grid(row=2, column=0, pady=16, sticky="ew")
         self.copy_content_frame._scrollbar.configure(width=12)
-
+        self.copy_content_frame.grid_columnconfigure(0, weight=1)
+        self.copy_content_frame.grid_rowconfigure(0, weight=1)
+        
         self.render_content()
         self.start_thread()
         self.hotkey_listener()
@@ -72,7 +96,6 @@ class App(ctk.CTk):
                 self.deiconify()
                 self.lift()
                 self.focus_force()
-                
             self.after(0, show)
         
         def listen():
@@ -104,57 +127,74 @@ class App(ctk.CTk):
         """
         Creates a row of buttons for each individual copied content.
         In a single row, there is the copy button and delete button.
-
-        TODO: Pin button (copy will stay locally on the computer)
         """
-
         for widget in self.copy_content_frame.winfo_children():
             widget.destroy()
 
-        for i, item in enumerate(self.copy_history):
-            display = item.strip()[:90] + "..." if len(item) > 90 else item.strip()
-
-            button_row = ctk.CTkFrame(self.copy_content_frame, fg_color="transparent")
-            button_row.grid(row=i, column=0, sticky="ew", padx=4, pady=2)
-            button_row.grid_columnconfigure(0, weight=1)
-
-            copy_btn = ctk.CTkLabel(
-                button_row,
-                text=display,
+        if not self.copy_history:
+            # Shows up when no items are in the copy history
+            empty_label = ctk.CTkLabel(
+                self.copy_content_frame,
+                text="Nothing copied yet!\nCopy (Ctrl+C) something and it will show up here.",
                 font=self.app_font,
-                width=343,
-                height=61,
-                anchor="nw",      
-                justify="left",
-                fg_color="grey",
-                text_color="white",
-                padx=7,
-                pady=7,
-                corner_radius=6
+                justify="center",
+                fg_color="transparent",
+                text_color="grey",
             )
-            copy_btn.grid(row=i, column=0, padx=3)
-            copy_btn.grid_propagate(False)
-            copy_btn.bind("<Button-1>", lambda e, val=item: self.copy_content(val))
-            copy_btn.bind("<Enter>", lambda e, btn=copy_btn: btn.configure(fg_color=("green", "blue")))
-            copy_btn.bind("<Leave>", lambda e, btn=copy_btn: btn.configure(fg_color="grey"))
-            
+            empty_label.grid(row=1, column=0, pady=20,  sticky="nsew")
+        
+        else:
+            for i, item in enumerate(self.copy_history):
+                display = item.strip()[:90] + "..." if len(item) > 90 else item.strip()
 
-            delete_btn = ctk.CTkButton(
-                button_row,
-                text="x",
-                width=25,
-                height=25,
-                anchor="center",
-                fg_color="red",
-                text_color=("black", "white"),
-                hover_color=("green", "blue"),
-                command=lambda val=item: self.delete_content(val)
-            )
-            delete_btn.grid(row=i, column=1)
+                button_row = ctk.CTkFrame(self.copy_content_frame, fg_color="transparent")
+                button_row.grid(row=i, column=0, sticky="ew", padx=4, pady=2)
+                button_row.grid_columnconfigure(0, weight=1)
+
+                copy_btn = ctk.CTkLabel(
+                    button_row,
+                    text=display,
+                    font=self.app_font,
+                    width=343,
+                    height=61,
+                    anchor="nw",      
+                    justify="left",
+                    fg_color="grey",
+                    text_color="white",
+                    padx=7,
+                    pady=7,
+                    corner_radius=6
+                )
+
+                copy_btn.grid_propagate(False)
+                copy_btn.grid(row=i+1, column=0, padx=3)
+                # e is used to consume Events. Without it, val and btn would be of type Event instead of String or CTkButton
+                copy_btn.bind("<Button-1>", lambda e, val=item: self.copy_content(val))
+                copy_btn.bind("<Enter>", lambda e, btn=copy_btn: btn.configure(fg_color=("green", "blue")))
+                copy_btn.bind("<Leave>", lambda e, btn=copy_btn: btn.configure(fg_color="grey"))
+                
+                delete_btn = ctk.CTkButton(
+                    button_row,
+                    text="❌",
+                    width=25,
+                    height=25,
+                    fg_color="red",
+                    text_color=("black", "white"),
+                    hover_color=("green", "blue"),
+                    command=lambda val=item: self.delete_content(val)
+                )
+                delete_btn.grid(row=i+1, column=1)
             
+    def clear_content(self):
+        """
+        Deletes all content from Paperweight's copy history
+        """
+        self.copy_history.clear()
+        self.render_content()
+
     def delete_content(self, value):
         """
-        Deletes specified copied content from Paperweight's list
+        Deletes specified copied content from Paperweight's copy history
         """
         self.copy_history.remove(value)
         self.render_content()
@@ -185,7 +225,7 @@ class App(ctk.CTk):
                 except Exception as e:
                     print(e)
                     pass #TODO: Could not be copied notif
-                time.sleep(0.5)
+                time.sleep(0.5) # Rate at which Paperweight updates the copy history (NOT renders)
         t = threading.Thread(target=poll, daemon=True)
         t.start()
 
