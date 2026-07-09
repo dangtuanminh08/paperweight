80f6d23d33ec58aae48b9433a623af95c0c89b74
dangtuanminh8 <dangtuanminh1123@gmail.com>
2026-07-09 11:44:07 -0400

    Final build More meaningful names, app icon. Ship incoming

--- paperweight.py
+++ paperweight.py
@@ -11,7 +11,10 @@ class App(ctk.CTk):
         self.title("Paperweight")
         self.overrideredirect(True) # Gets rid of the Windows title bar
 
-        self.app_font = ctk.CTkFont(family="Calibri", size=13)
+        self.app_font = ctk.CTkFont(family="Calibri", size=-13)
+        self.button_colorset = ("white", "#333333")  
+        self.text_colorset = ("black", "white")
+
         self.copy_history = [] 
 
         self.grid_columnconfigure(0, weight=1)
@@ -36,14 +39,26 @@ class App(ctk.CTk):
             height=35,
             corner_radius=0,
             fg_color="transparent",
+            text_color=self.text_colorset,
             hover_color="red",
-            command=self.destroy
+            command=self.withdraw
         )
 
         self.close_btn.grid(row=0, column=1, sticky="ne")
 
+        self.title_label = ctk.CTkLabel(
+                self.title_bar,
+                text="Paperweight",
+                font=self.app_font,
+                fg_color="transparent",
+                text_color=self.text_colorset,
+        )
+        self.title_label.grid(row=0, column=0, sticky="w", padx=15)
+
         self.title_bar.bind("<ButtonPress-1>", self.drag_start)
+        self.title_label.bind("<ButtonPress-1>", self.drag_start)
         self.title_bar.bind("<B1-Motion>", self.drag_motion)
+        self.title_label.bind("<B1-Motion>", self.drag_motion)
 
         # Utility Bar
         self.utility_bar = ctk.CTkFrame(
@@ -65,7 +80,8 @@ class App(ctk.CTk):
             width=60,
             height=30,
             corner_radius=6,
-            fg_color="red",
+            fg_color=self.button_colorset,
+            text_color=self.text_colorset,
             hover_color="blue",
             command=self.clear_content
         )
@@ -84,10 +100,13 @@ class App(ctk.CTk):
         self.copy_content_frame.grid_rowconfigure(0, weight=1)
         
         self.render_content()
-        self.start_thread()
-        self.hotkey_listener()
+        self.start_clipboard_listener()
+        self.start_hotkey_listener()
+
+    def change_appearanace(self):
+        ctk.set_appearance_mode(self.switch_var.get())
         
-    def hotkey_listener(self):
+    def start_hotkey_listener(self):
         """
         Allows Paperweight to listen for a specific keybind that will open the window.
         """
@@ -99,11 +118,11 @@ class App(ctk.CTk):
             self.after(0, show)
         
         def listen():
-            with keyboard.GlobalHotKeys({"<ctrl>+<shift>+b": on_press}) as h:
-                h.join()
+            with keyboard.GlobalHotKeys({"<ctrl>+<alt>+v": on_press}) as hotkey:
+                hotkey.join()
 
-        t = threading.Thread(target=listen, daemon=True)
-        t.start()
+        hotkey_thread = threading.Thread(target=listen, daemon=True)
+        hotkey_thread.start()
 
 
     def drag_start(self, event):
@@ -145,7 +164,7 @@ class App(ctk.CTk):
         
         else:
             for i, item in enumerate(self.copy_history):
-                display = item.strip()[:90] + "..." if len(item) > 90 else item.strip()
+                display = item.strip()[:150] + "..." if len(item) > 150 else item.strip()
 
                 button_row = ctk.CTkFrame(self.copy_content_frame, fg_color="transparent")
                 button_row.grid(row=i, column=0, sticky="ew", padx=4, pady=2)
@@ -156,11 +175,11 @@ class App(ctk.CTk):
                     text=display,
                     font=self.app_font,
                     width=343,
-                    height=61,
+                    height=90,
                     anchor="nw",      
                     justify="left",
-                    fg_color="grey",
-                    text_color="white",
+                    fg_color=self.button_colorset,
+                    text_color=self.text_colorset,
                     padx=7,
                     pady=7,
                     corner_radius=6
@@ -170,17 +189,17 @@ class App(ctk.CTk):
                 copy_btn.grid(row=i+1, column=0, padx=3)
                 # e is used to consume Events. Without it, val and btn would be of type Event instead of String or CTkButton
                 copy_btn.bind("<Button-1>", lambda e, val=item: self.copy_content(val))
-                copy_btn.bind("<Enter>", lambda e, btn=copy_btn: btn.configure(fg_color=("green", "blue")))
-                copy_btn.bind("<Leave>", lambda e, btn=copy_btn: btn.configure(fg_color="grey"))
+                copy_btn.bind("<Enter>", lambda e, btn=copy_btn: btn.configure(fg_color=("#F4F4F4", "#434343")))
+                copy_btn.bind("<Leave>", lambda e, btn=copy_btn: btn.configure(fg_color=self.button_colorset))
                 
                 delete_btn = ctk.CTkButton(
                     button_row,
-                    text="❌",
+                    text="🗑",
                     width=25,
-                    height=25,
-                    fg_color="red",
-                    text_color=("black", "white"),
-                    hover_color=("green", "blue"),
+                    height=45,
+                    fg_color=self.button_colorset,
+                    text_color=self.text_colorset,
+                    hover_color="red",
                     command=lambda val=item: self.delete_content(val)
                 )
                 delete_btn.grid(row=i+1, column=1)
@@ -205,7 +224,7 @@ class App(ctk.CTk):
         """
         pyperclip.copy(value)
         
-    def start_thread(self):
+    def start_clipboard_listener(self):
         """
         Allows Paperweight to listen for unique copy content that goes into the computer's clipboard
         without disrupting CustomTkinter's main loop.
@@ -222,12 +241,12 @@ class App(ctk.CTk):
                         if len(self.copy_history) > 100:
                             self.copy_history.pop(0)
                         self.after(0, self.render_content)
-                except Exception as e:
+                except pyperclip.PyperclipException as e:
                     print(e)
                     pass #TODO: Could not be copied notif
                 time.sleep(0.5) # Rate at which Paperweight updates the copy history (NOT renders)
-        t = threading.Thread(target=poll, daemon=True)
-        t.start()
+        clipboard_thread = threading.Thread(target=poll, daemon=True)
+        clipboard_thread.start()
 
 app = App()
 app.mainloop()
